06dd56390cbc2bc9af8af970ee6db975204bbef9
Eugene <evgenii.esaulenko@gmail.com>
2025-07-02 19:18:35 +0200

    Demo layout

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "imgui.h"
 #include "rlImGui.h"
 
+#include <algorithm>
+
 int main()
 {
 	int screenWidth = 1280;
@@ -12,9 +14,10 @@ int main()
 	SetTargetFPS(60);
 
 	rlImGuiSetup();
+    ImGui::GetIO().ConfigWindowsMoveFromTitleBarOnly = true;
 
 	Texture image = LoadTexture("resources/parrots.png");
-    bool open = true;
+    bool open = false;
 
 	while (!WindowShouldClose())
 	{
@@ -23,14 +26,73 @@ int main()
 
 		rlImGuiBegin();
 		{
-		    if (open)
+		    if (ImGui::BeginMainMenuBar())
 		    {
-		        ImGui::ShowDemoWindow(&open);
+		        if (ImGui::BeginMenu("File"))
+		        {
+		            if (ImGui::MenuItem("Open"))
+		            {
+		            }
+
+		            if (ImGui::MenuItem("Quit"))
+		            {
+		                CloseWindow();
+		            }
+		            ImGui::EndMenu();
+		        }
+
+		        if (ImGui::BeginMenu("Window"))
+		        {
+		            if (ImGui::MenuItem("Demo"))
+		            {
+		                open = !open;
+		            }
+		            ImGui::EndMenu();
+		        }
+
+		        ImGui::EndMainMenuBar();
 		    }
 
-		    if (ImGui::Begin("Test Window", nullptr))
+		    ImGuiViewport* viewport = ImGui::GetMainViewport();
+		    ImGui::SetNextWindowPos(viewport->WorkPos);
+		    ImGui::SetNextWindowSize(viewport->WorkSize);
+
+		    if (ImGui::Begin("Main Window", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoBringToFrontOnFocus))
 		    {
-		        rlImGuiImage(&image);
+		        if (open)
+		        {
+		            ImGui::ShowDemoWindow(&open);
+		        }
+
+		        float toolBarWidth = std::max(260.0f, ImGui::GetContentRegionAvail().x * 0.25f);
+		        if (ImGui::BeginChild("Toolbar", ImVec2(toolBarWidth, 0.0f)))
+		        {
+		            ImGui::Text("Details");
+		            ImGui::NewLine();
+		            ImGui::Text("Properties");
+		        }
+		        ImGui::EndChild();
+
+		        ImGui::SameLine();
+
+		        ImGui::PushStyleColor(ImGuiCol_ChildBg, ImVec4(0.1f, 0.1f, 0.1f, 0.9f));
+		        if (ImGui::BeginChild("Main View", ImVec2(0.0f, 0.0f), ImGuiChildFlags_None))
+		        {
+		            float width = ImGui::GetContentRegionAvail().x;
+		            float height = ImGui::GetContentRegionAvail().y;
+		            ImGui::Image(ImTextureID(image.id), ImVec2(width, height));
+		        }
+		        ImGui::EndChild();
+		        ImGui::PopStyleColor();
+
+		        // ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+		        // ImGui::SetNextWindowPos(center, ImGuiCond_Always, ImVec2(0.5f, 0.5f));
+		        // ImGui::SetNextWindowSize(ImVec2(550, 680), ImGuiCond_Always);
+		        // if (ImGui::Begin("Test Window", nullptr, ImGuiWindowFlags_AlwaysAutoResize))
+		        // {
+		        //
+		        // }
+		        // ImGui::End();
 		    }
 		    ImGui::End();
 		}
